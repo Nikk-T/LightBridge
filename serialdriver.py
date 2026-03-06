ebd70815c204fa560302967a77e20e8d1025b6d9
Nikk-T <nikolay@taganov.co.uk>
2026-03-06 21:17:56 +0000

    Relative path issue is fixed for CONFIG and LOG paths

--- serialdriver.py
+++ serialdriver.py
@@ -3,6 +3,22 @@ import asyncio, logging, serial, time
 from mdp_protocol import *
 log = logging.getLogger(__name__)
 
+#TRY Later
+""" # --------------------------------------------------
+# Serial connection manager
+# --------------------------------------------------
+async def connect_serial():
+    while True:
+        try:
+            ser = serial.Serial(SERIAL_PORT, SERIAL_BAUD, timeout=0.1)
+            log.info(f"Serial connected {SERIAL_PORT}")
+            return ser
+        except Exception as e:
+            log.error(f"Serial connect failed: {e}")
+            await asyncio.sleep(RECONNECT_DELAY)
+"""
+
+
 class SLS960:
     def __init__(self, port, baud):
         self.ser = serial.Serial(
@@ -18,7 +34,8 @@ class SLS960:
         self.ser.flush()
 
     def rgb(self, ch, r, g, b):
-        self.send(cmd_rgb_level(ch, r, g, b))
+        self.send(cmd_rgb_fade(ch, r, 10, 10, g, 10, 10, b, 10, 10))
+    #    self.send(cmd_rgb_level(ch, r, g, b))
 
     def off(self, ch):
         self.send(cmd_off(ch))
